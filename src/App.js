1908c3d76a6279b45f18d2e9f47c823198cb2613
6210110086 <6210110086@psu.ac.th>
2021-07-11 00:39:31 +0700

    card-game7

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
 import WordCard from './WordCard';
+const sad=()=>{
+  return {s:0};
+}
 
-const word = "Hello1";
 function App() {
+  const [state, setState] = useState(sad())
+  const z=state.s==1? "correct":state.s==2?"Incorrect":""
+  const check=value=>{
+    setState({s:1})
+  }
+  const f=()=>{
+    setState({s:2})
+  }
   return (
     <div>
-      {
-       <WordCard value="Hello"/>
-      }
+      <br></br>
+       <center><div>รูปภาพนี้คือมังงะเรื่องอะไร?</div></center><br></br>
+      <center><img src ="https://sololevelingthailand.com/wp-content/uploads/2021/04/Jogo-e-projeto-live-action-de-Solo-Leveling-1.jpg" width="280px" height="350px"/></center>
+      <center><div className="card" onClick={f}>One Piece</div></center>
+      <center><div className="card" onClick={f}>Naruto Shippuden</div></center>
+      <center><div className="card" onClick={check}>Solo leveling</div></center><br></br>
+      <center><div className="card activeCard">{z}</div></center>
     </div>
   );
 }
